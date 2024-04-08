ff1f405099907a52dfb354a56837a8b31d9cd68d
Dennis Garay <Dgaray2@lsu.edu>
2024-04-08 15:50:22 -0500

    test1

---
+++ MockData.js
@@ -0,0 +1,17 @@
+export const data = [
+    {"DeckNumber":1,"Name":"Alia","element":"Wood","role":"Subcontractor"},
+    {"DeckNumber":2,"Name":"Rebekkah","element":"Wood","role":"Supervisor"},
+    {"DeckNumber":3,"Name":"Gilbert","element":"Rubber","role":"Construction Foreman"},
+    {"DeckNumber":4,"Name":"Dana","element":"Wood","role":"Supervisor"},
+    {"DeckNumber":5,"Name":"Caspar","element":"Plastic","role":"Surveyor"},
+    {"DeckNumber":6,"Name":"Hildagarde","element":"Plastic","role":"Supervisor"},
+    {"DeckNumber":7,"Name":"Udell","element":"Plastic","role":"Subcontractor"},
+    {"DeckNumber":8,"Name":"Cacilia","element":"Rubber","role":"Estimator"},
+    {"DeckNumber":9,"Name":"Arabela","element":"Granite","role":"Supervisor"},
+    {"DeckNumber":10,"Name":"Gertrudis","element":"Plastic","role":"Architect"},
+    {"DeckNumber":11,"Name":"Byran","element":"Vinyl","role":"Architect"},
+    {"DeckNumber":12,"Name":"Dalli","element":"Steel","role":"Supervisor"},
+    {"DeckNumber":13,"Name":"Christiano","element":"Wood","role":"Estimator"},
+    {"DeckNumber":14,"Name":"Beau","element":"Steel","role":"Construction Foreman"},
+    {"DeckNumber":15,"Name":"Cyrillus","element":"Aluminum","role":"Electrician"}
+]

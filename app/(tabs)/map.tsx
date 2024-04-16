7029af0d40f38d0220b165e28bad82817e98520a
Jack Thomas <jtho487@lsu.edu>
2024-04-15 19:47:01 -0500

    Pal_Map2

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,39 +1,95 @@
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, SafeAreaView, Dimensions, ScrollView, StatusBar } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
+import { Stack } from 'expo-router';
 
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView, { UrlTile } from 'react-native-maps';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+// Define your options and corresponding image paths
+const optionImageMap: Record<string, any> = {
+  'Bosses': require('./assets/palmaps/palmap_bosses.png'),
+  'Enemy Camps': require('./assets/palmaps/palmap_camp.png'),
+  'Dungeons': require('./assets/palmaps/palmap_dungeons.png'),
+  'Fast Travel': require('./assets/palmaps/palmap_fasttravel.png'),
+  'Merchants': require('./assets/palmaps/palmap_merchant.png'),
+  'Memos': require('./assets/palmaps/palmap_note.png'),
+  'Skill Fruits': require('./assets/palmaps/palmap_npc.png'),
+  'Syndicate Towers': require('./assets/palmaps/palmap_tower.png'),
+};
+const options = Object.keys(optionImageMap);
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 0,
-          longitude: 0,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1,
-        }}
-      >
-        {/* Add your LocalTile layer */}
-  
-        <UrlTile
-            urlTemplate="https://api.maptiler.com/tiles/3499956a-d35c-43b0-9de0-0c91b3385332/?key=PjKvWXCShbMhKQQUKJgF#18.0/-34.60000/-58.38300"
-            maximumZ={19}
-          />
-      </MapView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor='#3A3B3C'/>
+      <Stack.Screen options={{ header: () => null}}/>
+      <SelectDropdown
+        data={options}
+        onSelect={(selectedItem, index) => setSelectedOption(options[index])}
+        buttonTextAfterSelection={(selectedItem) => selectedItem}
+        rowTextForSelection={(item) => item}
+        buttonStyle={styles.dropdownButton}
+        dropdownStyle={styles.dropdown}
+      />
+      <View style={styles.imageContainer}>
+        {selectedOption && (
+          <ScrollView
+            maximumZoomScale={2} // Adjust these values as needed
+            minimumZoomScale={1}
+            contentContainerStyle={styles.scrollViewContent}
+            horizontal={true}
+          >
+            <Image 
+              source={optionImageMap[selectedOption]} 
+              style={[styles.image, { minHeight: windowHeight * 0.8, minWidth: windowWidth * 0.8}]} 
+            />
+          </ScrollView>
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  map: {
+  dropdownButton: {
+    width: 200,
+    height: 40,
+    backgroundColor: 'lightgray',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  dropdown: {
+    width: 200,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  imageContainer: {
     flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    resizeMode: 'contain',
+    minWidth: windowWidth,
+    minHeight: windowHeight,
   },
 });
 
 export default App;
-
--- app/(tabs)/pals.tsx
+++ app/(tabs)/pals.tsx
@@ -7,34 +7,129 @@ import {
     StatusBar,
     SafeAreaView,
     Linking,
+    Button,
+    Pressable,
     FlatList,
   } from "react-native";
-import React from 'react';
-import { Stack, router, } from "expo-router";
+import React, { useEffect, useState } from 'react';
+import { Octicons } from '@expo/vector-icons';
+import { Link, Stack, router, } from "expo-router";
+import SelectDropdown from "react-native-select-dropdown";
+import { AntDesign, FontAwesome5, FontAwesome, Ionicons, Feather } from '@expo/vector-icons';
+import * as SQLite from 'expo-sqlite';
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
 
+interface Pal {
+    label: any;
+    value: any;
+}
 
-const pals = () => {
-    return ( 
-        <SafeAreaView style={styles.conatiner}>
+const db = SQLite.openDatabase('PalDeckv2.db');
 
-        </SafeAreaView>
-    );
+const PalInfoDropdown = () => {
+  const [pals, setPals] = useState<Pal[]>([]);
+  const [selectedPal, setSelectedPal] = useState<Pal | null>(null);
+
+  useEffect(() => {
+    loadDB();
+    loadPalsFromDatabase();
+  }, []);
+
+  const loadDB = async () => {
+    if (!(await FileSystem.getInfoAsync('file://Palpedia')).exists) {
+      await FileSystem.makeDirectoryAsync('file://Palpedia');
+    }
+    await FileSystem.downloadAsync('file://app/(tabs)/PalDeckv2.db', 'file://Palpedia/app');
+  };
+
+  const loadPalsFromDatabase = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM data',
+        [],
+        (_, { rows }) => {
+          const palsArray: Pal[] = rows._array.map((row: any) => ({
+            label: row.Name,
+            value: `Type 1: ${row['Type 1']}, Type 2: ${row['Type 2']}`, // change to display whatever needed
+          }));
+          setPals(palsArray);
+        },
+        (_, error) => {
+          console.error('Error fetching pals:', error);
+          return true;
+        }
+      );
+    });
+  };
+
+  const onSelectPal = (label: string) => {
+    // Find the selected pal by label
+    const selectedPal = pals.find((pal) => pal.label === label);
+    if (selectedPal) {
+      setSelectedPal(selectedPal);
+    } else {
+      console.error('Invalid label:', label);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>Pal-Pedia</Text>
+      <SelectDropdown
+        data={pals.map((pal) => pal.label)}
+        onSelect={onSelectPal}
+        defaultButtonText="Select a Pal"
+        buttonTextAfterSelection={(selectedItem, index) => pals[index].label}
+        buttonStyle={styles.dropdownButton}
+        buttonTextStyle={styles.dropdownButtonText}
+        dropdownStyle={styles.dropdown}
+        search
+      />
+      {selectedPal && (
+        <View style={styles.itemInfoContainer}>
+          <Text style={styles.itemInfoText}>{selectedPal.value}</Text>
+        </View>
+      )}
+    </SafeAreaView>
+  );
 };
 
-
-
 const styles = StyleSheet.create({
-    conatiner: {
-        flex: 1,
-        marginBottom: 0,
-    },
-    FlatList: {
-        backgroundColor: '#272525',
-        marginHorizontal: 0,
-        marginVertical: 0,
-      },
-
-
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#272525',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+  },
+  dropdownButton: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  dropdownButtonText: {
+    fontSize: 16,
+  },
+  dropdown: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+  },
+  itemInfoContainer: {
+    marginTop: 20,
+  },
+  itemInfoText: {
+    fontSize: 18,
+    color: '#fff',
+  },
 });
 
-export default pals;
+export default PalInfoDropdown;

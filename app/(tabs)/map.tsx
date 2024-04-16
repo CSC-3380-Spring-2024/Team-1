c9efa6949f6fbad2f4709fd79ea1826b835e2553
Jack Thomas <jtho487@lsu.edu>
2024-04-15 19:42:50 -0500

    Pal_Maps / Selection Screen Code

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,45 +1,39 @@
-import { 
-    View,
-    Text, 
-    Image,
-    StyleSheet,
-    ScrollView,
-    StatusBar,
-    SafeAreaView,
-    Linking,
-    Platform,
-  } from "react-native";
+
 import React from 'react';
-import { Stack, router, } from "expo-router";
-import MapView, { UrlTile } from "react-native-maps";
+import { View, StyleSheet } from 'react-native';
+import MapView, { UrlTile } from 'react-native-maps';
 
-
-const map = () => {
-    return ( 
-        <View style={styles.container}>
+const App = () => {
+  return (
+    <View style={styles.container}>
       <MapView
         style={styles.map}
-        
+        initialRegion={{
+          latitude: 0,
+          longitude: 0,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
       >
+        {/* Add your LocalTile layer */}
+  
         <UrlTile
-          urlTemplate="https://api.maptiler.com/tiles/3d55ff57-ba59-442d-8e07-e55c30a2dd7b/?key=7kzpPw2oc7jNaFZdRLyk#7.7"
-          maximumZ={19}
-        />
+            urlTemplate="https://api.maptiler.com/tiles/3499956a-d35c-43b0-9de0-0c91b3385332/?key=PjKvWXCShbMhKQQUKJgF#18.0/-34.60000/-58.38300"
+            maximumZ={19}
+          />
       </MapView>
     </View>
-    );
+  );
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    }
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
 });
 
-export default map;
+export default App;
+
